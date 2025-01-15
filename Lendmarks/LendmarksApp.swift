4d71804278274eaace538156b4be5edc21b87e69
Filippov Konstantin <fkv.katafey@yandex.ru>
2025-01-15 12:58:10 +0400

    Initial Commit

---
+++ Lendmarks/LendmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LendmarksApp.swift
+//  Lendmarks
+//
+//  Created by Константин Филиппов on 15.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct LendmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LendmarksTests/LendmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  LendmarksTests.swift
+//  LendmarksTests
+//
+//  Created by Константин Филиппов on 15.01.2025.
+//
+
+import Testing
+@testable import Lendmarks
+
+struct LendmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
