f9b2f7df4bc65a7816f1c18d73a9dabb96f1cd95
Filippov Konstantin <fkv.katafey@yandex.ru>
2025-01-16 11:02:07 +0400

    Изменил название

--- Lendmarks/ContentView.swift
+++ Lendmarks/ContentView.swift
@@ -3,12 +3,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Привет, Константин!")
+                .font(.title)
+                .foregroundColor(.green)
         }
-        .padding()
     }
 }
 
